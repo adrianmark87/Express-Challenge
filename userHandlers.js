a26c0887e078891fac0776654524e3191b4353fe
adrianmark87 <adrianmarculescu.ing@gmail.com>
2023-05-25 12:43:13 +0200

    Challenge 4 _ Update Done

--- userHandlers.js
+++ userHandlers.js
@@ -1,84 +1,104 @@
 const users = [
-    {
-      id: 1,
-      firstname: "Citizen Kane",
-      lastname: "Orson Wells",
-      email: "citizen@gmail.com",
-      city: 'London',
-      language: 'English',
-    },
-    {
-        id: 1,
-        firstname: "Maria",
-        lastname: "Mary",
-        email: "maria@gmail.com",
-        city: 'New York',
-        language: 'English',
-    },
-    {
-        id: 1,
-        firstname: "Orlando",
-        lastname: "Magic",
-        email: "magic@gmail.com",
-        city: 'Naples',
-        language: 'Italian',
-    },
-  ];
-  
-  const database = require("./database");
-  
-  const getUsers = (req, res) => {
-    database
-      .query("select * from users")
-      .then(([users]) => {
-        res.json(users);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error retrieving data from database");
-      });
-  };
-  
-  const getUserById = (req, res) => {
-    const id = parseInt(req.params.id);
-  
-     database
-      .query("select * from users where id = ?", [id])
-      .then(([users]) => {
-        if (users[0] != null) {
-          res.json(users[0]);
-        } else {
-          res.status(404).send("Not Found");
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error retrieving data from database");
-      });
-  };
+  {
+    id: 1,
+    firstname: "Citizen Kane",
+    lastname: "Orson Wells",
+    email: "citizen@gmail.com",
+    city: "London",
+    language: "English",
+  },
+  {
+    id: 1,
+    firstname: "Maria",
+    lastname: "Mary",
+    email: "maria@gmail.com",
+    city: "New York",
+    language: "English",
+  },
+  {
+    id: 1,
+    firstname: "Orlando",
+    lastname: "Magic",
+    email: "magic@gmail.com",
+    city: "Naples",
+    language: "Italian",
+  },
+];
 
-  const postUser = (req, res) => {
-    const { firstname, lastname, email, city, language } = req.body;
-  
-    database
-      .query(
-        "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-        [firstname, lastname, email, city, language]
-      )
-      .then(([result]) => {
-        res.location(`/api/users/${result.insertId}`).sendStatus(201);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error saving the user");
-      });
-  };
-  
-  module.exports = {
-    getUsers,
-    getUserById,
-    postUser,
-  };
-  
-  
-  
+const database = require("./database");
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the user");
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  postUser,
+  updateUser,
+};
