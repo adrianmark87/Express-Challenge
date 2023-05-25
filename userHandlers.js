683d40c9ffaa10fd5ed9dfeb9cd19921787a601c
adrianmark87 <adrianmarculescu.ing@gmail.com>
2023-05-25 12:13:07 +0200

    Challenge 3 Express - Post Done

--- userHandlers.js
+++ userHandlers.js
@@ -56,10 +56,28 @@ const users = [
         res.status(500).send("Error retrieving data from database");
       });
   };
+
+  const postUser = (req, res) => {
+    const { firstname, lastname, email, city, language } = req.body;
+  
+    database
+      .query(
+        "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+        [firstname, lastname, email, city, language]
+      )
+      .then(([result]) => {
+        res.location(`/api/users/${result.insertId}`).sendStatus(201);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error saving the user");
+      });
+  };
   
   module.exports = {
     getUsers,
     getUserById,
+    postUser,
   };
   
   
