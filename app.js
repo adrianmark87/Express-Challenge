5ea1dd86a07906b23baf895156c71a5b5cf394be
adrianmark87 <adrianmarculescu.ing@gmail.com>
2023-05-25 15:33:08 +0200

    Challenge 4bis Validator Done

--- app.js
+++ app.js
@@ -15,22 +15,29 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const { validateMovie } = require("./validators.js");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
-app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/movies", validateMovie,movieHandlers.postMovie);
 
-app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.put("/api/movies/:id", validateMovie,movieHandlers.updateMovie);
+
+
 
 const userHandlers = require("./userHandlers");
+
+const { validateUser } = require("./validators.js");
+
+app.post("/api/users", validateUser, userHandlers.postUser);
+
+app.put("/api/users/:id", validateUser, userHandlers.updateUser);
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
-app.post("/api/users", userHandlers.postUser);
 
-app.put("/api/users/:id", userHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
